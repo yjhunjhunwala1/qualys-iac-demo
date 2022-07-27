f3424d5d385fa5cd68094e15c14c7fe3e70c8dc0
Yash Jhunjhunwala <109724146+yjhunjhunwala1@users.noreply.github.com>
2022-07-27 14:52:45 +0530

    Update aws_iam_account_password_policy.tf

--- aws_iam_account_password_policy.tf
+++ aws_iam_account_password_policy.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_account_password_policy" "polciy" {
-  minimum_password_length      = 14
+  minimum_password_length      = 7
   require_lowercase_characters = true
   require_numbers              = true
   require_uppercase_characters   = true
