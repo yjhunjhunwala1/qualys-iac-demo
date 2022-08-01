2b17ee822ef759bc7d0e1cffd5ab5fb461e6085a
Yash Jhunjhunwala <109724146+yjhunjhunwala1@users.noreply.github.com>
2022-08-01 12:06:16 +0530

    Update and rename security_groups.tf to aws_security_group.tf

---
+++ aws/aws_security_group.tf
@@ -0,0 +1,25 @@
+{}
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
