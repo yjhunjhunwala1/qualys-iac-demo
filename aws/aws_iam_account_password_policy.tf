13e2dc6160ee79c9285fa5312de43b217a46de31
Yash Jhunjhunwala <109724146+yjhunjhunwala1@users.noreply.github.com>
2022-08-01 11:42:00 +0530

    Create aws_iam_account_password_policy.tf

---
+++ aws/aws_iam_account_password_policy.tf
@@ -0,0 +1,10 @@
+resource "aws_iam_account_password_policy" "polciy" {
+  minimum_password_length      = 6
+  require_lowercase_characters = false
+  require_numbers              = false
+  require_uppercase_characters   = false
+  require_symbols                = false
+  allow_users_to_change_password = false
+  password_reuse_prevention      = 1
+  max_password_age               = 1
+}
