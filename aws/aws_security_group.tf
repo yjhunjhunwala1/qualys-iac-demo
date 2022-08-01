b92bfcd33ad7475cdaa8dce1bab065c68fe7a703
Yash Jhunjhunwala <109724146+yjhunjhunwala1@users.noreply.github.com>
2022-08-01 12:14:49 +0530

    Update aws_security_group.tf

--- aws/aws_security_group.tf
+++ aws/aws_security_group.tf
@@ -4,15 +4,15 @@ resource "aws_security_group" "allow_tls" {
   description = "Allow TLS inbound traffic"
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
+    description      = "Public RDP"
+    from_port        = 3389
+    to_port          = 3389
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
   ingress {
-    description      = "TLS from VPC"
+    description      = "Pubic SSH"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
